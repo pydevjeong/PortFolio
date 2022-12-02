4473a6deedaa63220e3f4b05559688dde5d381dc
pydevjeong <njh3421@gmail.com>
2022-12-02 21:26:36 +0900

    Start making gallery

---
+++ src/components/ProjectPage/Gallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FirstProject from './FirstProject'
+const Gallery = () => {
+  return (
+    <div>
+      <FirstProject/>
+    </div>
+  );
+};
+
+export default Gallery;
